b078e9074aa9d6f0cf19a1ca0298db23db7aa052
Javier Orozco <desarrollotic1@ttncompany.com>
2024-10-26 12:27:54 -0500

    feat: implemeting appointment feature using action decorator

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from bookings.models import Appointment
+from bookings.serializers import AppointmentSerializer
 from doctors.serializers import DoctorSerializer
 from doctors.models import Doctor
 from doctors.permissions import IsDoctor
@@ -28,3 +31,24 @@ class DoctorViewSet(viewsets.ModelViewSet):
         doctor.is_on_vacation = False
         doctor.save()
         return Response({"status": "El doctor NO está en vacaciones"})
+
+    @action(['POST', 'GET'], detail=True, serializer_class=AppointmentSerializer)
+    def appointment(self, request, pk):
+        doctor = self.get_object()
+
+        if request.method == 'POST':
+            data = request.data.copy()
+            data['doctor'] = doctor.id
+            serializer = AppointmentSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'GET':
+            appointments = Appointment.objects.filter(
+                doctor=doctor
+            )
+            serializer = AppointmentSerializer(appointments, many=True)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
